4934b1443d5e741282854d6956dac6b7f565903c
SmCTwelve <smctwelve@gmail.com>
2019-01-14 20:50:38 +0000

    Fixed conn close error, handle all response logic within context block.

--- f1/fetch.py
+++ f1/fetch.py
@@ -9,25 +9,39 @@ SESSION_TIMEOUT = 30
 logger = logging.getLogger(__name__)
 
 
+def is_xml(res): return 'application/xml' in res.content_type
+
+
+def is_json(res): return 'application/json' in res.content_type
+
+
 async def send_request(session, url):
-    '''Attempt to request the URL. Returns response object if successful.'''
+    '''Attempt to request the URL. Returns response object if successful or None.'''
     logger.info('GET {}'.format(url))
+    # open connection context, all response handling must be within
     async with session.get(url) as res:
         logger.info('Response HTTP/{}'.format(res.status))
-        if res.status == 200:
-            return res
-        else:
-            logger.warn('Problem fetching request. Failed with HTTP/{} {}'.format(res.status, res.reason))
+        if res.status != 200:
+            logger.warning('Problem fetching request. Failed with HTTP/{} {}'.format(res.status, res.reason))
             return None
+        else:
+            if is_xml(res):
+                print('XML')
+                content = await res.read()
+            elif is_json(res):
+                content = await res.json()
+            else:
+                content = await res.text()
+            return content
 
 
 async def fetch(url):
-    '''Request the url and await response.'''
+    '''Request the url and await response. Returns response content or None.'''
     tmout = aiohttp.ClientTimeout(total=SESSION_TIMEOUT)
     try:
         async with aiohttp.ClientSession(timeout=tmout) as session:
             res = await send_request(session, url)
-            return await res.text()
+            return res
     except aiohttp.ClientError as e:
         logger.error(e)
         return None
