f7f38055ad835930ff07605a495d3b02c9791c7b
SmCTwelve <smctwelve@gmail.com>
2019-01-14 20:52:09 +0000

    Parse XML with bs4, helper functions

--- f1/f1.py
+++ f1/f1.py
@@ -2,11 +2,12 @@
 Utilities to grab latest F1 results from Ergast API.
 '''
 import logging
-from f1.fetch import fetch
+import asyncio
+from fetch import fetch
 from bs4 import BeautifulSoup
 from tabulate import tabulate
 
-BASE_URL = 'http://ergast.com/api/f1/current/'
+BASE_URL = 'http://ergast.com/api/f1/current'
 
 # -- Use caching for all requests to reuse results from previous command use, only request directly from API if expired --
 # -- Check if race weekend, lower cache period --
@@ -21,22 +22,51 @@ BASE_URL = 'http://ergast.com/api/f1/current/'
 # !f1 update -- ADMIN, manually reset cache
 # !f1 help | <command> help -- help text and usage example
 
+logging.basicConfig(
+    level=logging.INFO,
+    format='[%(asctime)s][%(name)s] %(levelname)s: %(message)s'
+)
+logger = logging.getLogger(__name__)
+
+
+async def get_soup(url):
+    '''Request the URL and parse response as BeautifulSoup object.'''
+    res = await fetch(url)
+    if res is None:
+        logger.warning('Unable to get soup, response was None.')
+        return None
+    return BeautifulSoup(res, 'lxml')
+
 
 async def get_driver_standings():
+    '''Returns the latest driver championship standings.
+
+    Fetches results from API. Response XML is parsed into a list of dicts to be tabulated.
+    Data includes position, driver code, total points and wins.
+    '''
     url = f'{BASE_URL}/driverStandings'
-    soup = BeautifulSoup(await fetch(url), 'lxml')
-    standings = soup.find('StandingsList')
-    result = []
-    for standing in standings.children:
-        result.append(
-            {
-                'Pos': standing['position'],
-                'Driver': standing.Driver['code'],
-                'Points': standing['points'],
-                'Wins': standing['wins'],
-            }
-        )
-    return result
+    soup = await get_soup(url)
+    if soup:
+        # tags are lowercase
+        standings = soup.standingslist
+        result = {
+            'season': standings['season'],
+            'round': standings['round'],
+            'data': [],
+        }
+        for standing in standings.find_all('driverstanding'):
+            print(standing)
+            result['data'].append(
+                {
+                    'Pos': standing['position'],
+                    'Driver': standing.driver['code'],
+                    'Points': standing['points'],
+                    'Wins': standing['wins'],
+                }
+            )
+        print(tabulate(result['data'], headers='keys', tablefmt='grid'))
+        return result
+    return None
 
 
 async def get_team_standings():
@@ -49,3 +79,6 @@ async def get_all_drivers_and_teams():
     # For driver in drivers:
     #   {name, team, age, code, etc.}
     # Easy automated updating
+    pass
+
+asyncio.run(get_driver_standings())
