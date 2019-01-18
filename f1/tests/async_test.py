65d25820fcee21b0f5af0e9e7cbd8c098dd92302
SmCTwelve <smctwelve@gmail.com>
2019-01-18 17:14:31 +0000

    Updated description

--- f1/tests/async_test.py
+++ f1/tests/async_test.py
@@ -1,16 +1,18 @@
-'''Asynchronous test wrapper. Runs the test case in a new event loop and returns the awaitable Future.
-
-Example:
-
-```@async_test
-async test_method(self):
-    await self.async_task()
-```
-'''
+'''Asynchronous test wrapper.'''
 import asyncio
 
 
 def async_test(coro):
+    '''Runs the test case as a coroutine in a new event loop. Use as decorator to the test function.
+
+    Example:
+
+    ```
+    @async_test
+    async def test_method(self):
+        await self.async_task()
+    ```
+    '''
     def wrapper(*args, **kwargs):
         loop = asyncio.new_event_loop()
         return loop.run_until_complete(coro(*args, **kwargs))
