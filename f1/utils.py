1e0237b37499f3d2112beed28e25bcb603ecef43
SmCTwelve <smctwelve@gmail.com>
2019-01-19 04:56:54 +0000

    countdown() returns text and tuple. make_table() now takes format arg

--- f1/utils.py
+++ f1/utils.py
@@ -10,8 +10,8 @@ def contains(first, second):
     return any(i in first for i in second)
 
 
-def make_table(data, headers='keys'):
-    return tabulate(data, headers=headers, tablefmt='fancy_grid')
+def make_table(data, headers='keys', fmt='fancy_grid'):
+    return tabulate(data, headers=headers, tablefmt=fmt)
 
 
 def age(yob):
@@ -25,15 +25,25 @@ def date_parser(date_str):
 
 
 def time_parser(time_str):
-    return datetime.strptime(time_str, '%H:%M:%SZ').strftime('%X')
+    time = datetime.strptime(time_str, '%H:%M:%SZ').strftime('%X')
+    # Strip seconds
+    return time[:len(time) - 3]
 
 
 def countdown(target: datetime):
     '''
     Calculate time to `target` datetime object from current time when invoked.
-    Returns string tuple as (days, hrs, mins, sec).
+    Returns a list containing the string output and tuple of (days, hrs, mins, sec).
     '''
     delta = target - datetime.now()
     d = str(delta.days) if delta.days > -1 else 0
     h, m, s = str(delta).split(', ')[1].split(':')
-    return (d, h, m, s)
+    # trim ms
+    s = s.split('.')[0]
+    stringify = (
+        f"{d} {'days' if d is not '1' else 'day'}, "
+        f"{h} {'hours' if h is not '1' else 'hour'}, "
+        f"{m} {'minutes' if m is not '1' else 'minute'}, "
+        f"{s} {'seconds' if s is not '1' else 'second'} "
+    )
+    return [stringify, (d, h, m, s)]
