110caafe0ca53b148a93402cfc3f8c7b07a7f4e5
SmCTwelve <smctwelve@gmail.com>
2019-01-19 21:42:32 +0000

    Added permission checks

--- f1/admin.py
+++ f1/admin.py
@@ -1,12 +1,14 @@
 '''Administrator restricted commands.'''
 import logging
+from discord.ext import commands
 
 from f1.commands import bot
 
 logger = logging.getLogger(__name__)
 
 
-@bot.command
+@bot.command()
+@commands.is_owner()
 async def stop(ctx):
     '''Stop the running bot process.
 
@@ -16,11 +18,19 @@ async def stop(ctx):
     pass
 
 
-@bot.command
+@bot.command()
+@commands.is_owner()
 async def restart(ctx):
     '''Command the worker process to restart.
 
     The bot will be temporarily unavailable as the dyno reboots. A ready message
     will be posted when the bot can receive commands again.
     '''
+    pass
+
+
+@bot.command()
+@commands.is_owner()
+async def status(ctx, *args):
+    '''Current worker status. Returns the worker process, dyno hours, uptime.'''
     pass
