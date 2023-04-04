6137f3d6c1e4c9b01eab22e540a27cd7b9d6b2f7
SmCTwelve <smctwelve@gmail.com>
2023-04-04 12:14:06 +0100

    Get message send target from command ctx.

---
+++ f1/target.py
@@ -0,0 +1,31 @@
+from discord import ApplicationContext
+from discord.ext.commands import Context
+
+from f1.config import CONFIG
+
+
+class MessageTarget:
+    """Uses the appropriate response target based on the command context and config settings.
+
+    Can send messages to user as DM, reply to the message using `commands.Context` or use an `Interaction.response`
+    when used with an `ApplicationContext`.
+    """
+
+    def __init__(self, ctx: Context | ApplicationContext):
+        if not (isinstance(ctx, (Context, ApplicationContext))):
+            raise ValueError("No context available for message target.")
+        self.ctx = ctx
+
+    def send(self, *args, **kwargs):
+        return self._get_send()(*args, **kwargs)
+
+    def _get_send(self):
+        """Return a reference to the send method to use for the context."""
+        # Target DM channel
+        if CONFIG["MESSAGE"]["DM"]:
+            return self.ctx.author.send
+        # Use Application response for slash commands
+        if isinstance(self.ctx, ApplicationContext):
+            return self.ctx.respond
+        # Use normal reply for message commands
+        return self.ctx.reply
