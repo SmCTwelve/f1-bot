4376f014236196cecf658bfdff6cc3c9a7f9078d
SmCTwelve <smctwelve@gmail.com>
2023-05-03 10:56:01 +0100

    Revert bot race grouping

--- f1/cogs/race.py
+++ f1/cogs/race.py
@@ -1,19 +1,24 @@
 import asyncio
 import logging
-from discord import Colour, Embed, Option
+from discord import Colour, Embed, Option, command
 import discord
 from discord.ext import commands
 
 from f1 import utils
-from f1.api import ergast
+from f1.api import ergast, stats
 from f1.errors import DriverNotFoundError
 from f1.target import MessageTarget
 from f1.config import Config
 
 logger = logging.getLogger('f1-bot')
 
-SeasonOption = Option(str, default="current", description="Season year. If not specified uses current season.")
-RoundOption = Option(str, default="last", description="Race number. Most recent used if left out.")
+SeasonOption = Option(str, default="current", description="The season year. Leave blank for the current season.")
+RoundOption = Option(str, default="last", description="The race name, location or round number. Default is lastest.")
+SessionOption = Option(
+    str,
+    choices=["Practice 1", "Practice 2", "Practice 3", "Qualifying", "Sprint", "Sprint Shootout",
+             "Race"],
+    default="Race", description="The session to view.")
 RankedPitstopFilter = Option(str, choices=["Best", "Worst", "Ranked"], default="Ranked")
 LaptimeFilter = Option(str, choices=["Fastest", "Slowest", "Top 5", "Bottom 5", "Ranked"], default="Ranked")
 
@@ -24,21 +29,16 @@ class Race(commands.Cog, guild_ids=Config().guilds):
     def __init__(self, bot: discord.Bot):
         self.bot = bot
 
-    race = discord.SlashCommandGroup(
-        name="race",
-        description="Race related commands"
-    )
-
-    @race.command(description="Qualifying results. Default latest.")
-    async def qualifying(self, ctx, season: SeasonOption, round: RoundOption):
+    @commands.slash_command(description="Qualifying results. Default latest.")
+    async def qualifying(self, ctx, year: SeasonOption, round: RoundOption):
         """Qualifying results for the given race.
 
         Usage:
         ----------
             /qualifying [season] [round]
         """
-        await utils.check_season(ctx, season)
-        result = await ergast.get_qualifying_results(round, season)
+        await utils.check_season(ctx, year)
+        result = await ergast.get_qualifying_results(round, year)
         table = utils.make_table(result['data'])
         target = MessageTarget(ctx)
 
@@ -47,26 +47,30 @@ class Race(commands.Cog, guild_ids=Config().guilds):
             embed=Embed(title=f"**Qualifying Results - {result['race']} ({result['season']})**", url=result['url'])
         )
 
-    @race.command(description="Final race results. Default latest.")
-    async def results(self, ctx, season: SeasonOption, round: RoundOption):
-        """Get the complete race results. Both choices optional.
+    @commands.slash_command(description="Result data for the session. Default last race.")
+    async def results(self, ctx, year: SeasonOption, round: RoundOption, session: SessionOption):
+        """Get the results for a session.
 
         Usage:
         ----------
-            /results [season] [round]
+            /results [year] [round] [session]
         """
-        await utils.check_season(ctx, season)
-        result = await ergast.get_race_results(round, season)
-        table = utils.make_table(result['data'], fmt='simple')
         target = MessageTarget(ctx)
+        await utils.check_season(ctx, year)
+
+        # Load and format API data
+        ev = await stats.to_event(year, round)
+        s = stats.load_session(ev, session)
+        data = stats.format_results(s, session)
+        table = utils.make_table(data, fmt="simple", showindex=False)
 
         await target.send(
             content=f"```\n{table}\n```",
-            embed=Embed(title=f"**Race Results - {result['race']} ({result['season']})**", url=result['url']),
+            embed=Embed(title=f"**{s.name} - {ev.EventName} ({ev.EventDate.year})**"),
         )
 
-    @race.command(description="Race pitstops ranked by duration.", name="pitstops-ranked")
-    async def pitstops_ranked(self, ctx, season: SeasonOption, round: RoundOption, filter: RankedPitstopFilter):
+    @commands.slash_command(description="Race pitstops ranked by duration.", name="pitstops-ranked")
+    async def pitstops_ranked(self, ctx, year: SeasonOption, round: RoundOption, filter: RankedPitstopFilter):
         """Display pitstops for the race ranked by `filter`.
 
         All parameters are optional. Defaults to all pitstops ranked best to worst for the most recent race.
@@ -78,14 +82,14 @@ class Race(commands.Cog, guild_ids=Config().guilds):
         """
         target = MessageTarget(ctx)
         # Pit data only available from 2012 so catch seasons before
-        if not season == 'current':
-            if int(season) < 2012:
+        if not year == 'current':
+            if int(year) < 2012:
                 await ctx.send("Pitstop data not available before 2012.")
                 raise commands.BadArgument(message="Tried to get pitstops before 2012.")
-        await utils.check_season(ctx, season)
+        await utils.check_season(ctx, year)
 
-        # Get stops and stort them
-        res = await ergast.get_pitstops(round, season)
+        # Get stops and sort them
+        res = await ergast.get_pitstops(round, year)
         sorted_res = utils.rank_pitstops(res)
 
         # Filter based on choice
@@ -106,9 +110,9 @@ class Race(commands.Cog, guild_ids=Config().guilds):
         )
         await target.send(embed=emd)
 
-    @race.command(description="Driver pitstops.", name="pitstops-driver")
+    @commands.slash_command(description="Driver pitstops.", name="pitstops-driver")
     async def pitstops_driver(self, ctx, driver_id: Option(str, required=True),
-                              season: SeasonOption, round: RoundOption):
+                              year: SeasonOption, round: RoundOption):
         """Pitstops for a specific driver in the race.
 
         Both `season` and `round` are optional. `driver_id` is required and must be a driver number or 3 letter code.
@@ -123,14 +127,14 @@ class Race(commands.Cog, guild_ids=Config().guilds):
         """
         target = MessageTarget(ctx)
         # Check for post-2012 data
-        if not season == 'current':
-            if int(season) < 2012:
+        if not year == 'current':
+            if int(year) < 2012:
                 await ctx.send("Pitstop data not available before 2012.")
                 raise commands.BadArgument(message="Tried to get pitstops before 2012.")
-        await utils.check_season(ctx, season)
+        await utils.check_season(ctx, year)
 
         # Get driver stops
-        res = await ergast.get_pitstops(round, season)
+        res = await ergast.get_pitstops(round, year)
         try:
             driver = ergast.get_driver_info(driver_id)
             filtered = [s for s in res['data'] if s['Driver'] == driver['code']]
@@ -145,8 +149,8 @@ class Race(commands.Cog, guild_ids=Config().guilds):
             description=f"```\n{table}\n```"
         ))
 
-    @race.command(description="Best ranked lap times per driver in the race.")
-    async def laptimes(self, ctx, season: SeasonOption, round: RoundOption, filter: LaptimeFilter):
+    @commands.slash_command(description="Best ranked lap times per driver in the race.")
+    async def laptimes(self, ctx, year: SeasonOption, round: RoundOption, filter: LaptimeFilter):
         """Best ranked lap times per driver in the race. All parameters optional.
 
         Only the best recorded lap for each driver in the race.
@@ -157,8 +161,8 @@ class Race(commands.Cog, guild_ids=Config().guilds):
             /laptimes 2022 5 fastest
         """
         target = MessageTarget(ctx)
-        await utils.check_season(ctx, season)
-        res = await ergast.get_best_laps(round, season)
+        await utils.check_season(ctx, year)
+        res = await ergast.get_best_laps(round, year)
         sorted_times = utils.rank_best_lap_times(res)
 
         if filter == "Ranked":
@@ -178,7 +182,7 @@ class Race(commands.Cog, guild_ids=Config().guilds):
             description=f"```\n{table}\n```"
         ))
 
-    @race.command(description="Details and countdown to the next race weekend.")
+    @commands.slash_command(description="Details and countdown to the next race weekend.")
     async def next(self, ctx):
         result = await ergast.get_next_race()
         page_url = str(result['url']).replace(f"{result['season']}_", '')
