87f76098cbd9f19b31103ce0f29fed3ffd5a6667
SmCTwelve <smctwelve@gmail.com>
2023-05-16 18:23:09 +0100

    Use aiohttp caching

--- f1/api/fetch.py
+++ f1/api/fetch.py
@@ -1,21 +1,42 @@
 """
 Perform asyncronous web requests.
 """
-import aiohttp
 import logging
+from datetime import timedelta
 
+import aiohttp
+from aiohttp_client_cache import CachedSession, SQLiteBackend
+
+from f1.config import CACHE_DIR
+
+BASE_URL = 'http://ergast.com/api/f1'
 SESSION_TIMEOUT = 120
 
 logger = logging.getLogger(__name__)
 
+# Disable caching e.g. for testing
+use_cache = True
 
-def is_xml(res): return 'application/xml' in res.content_type
+cache = SQLiteBackend(
+    cache_name=f"{CACHE_DIR}/fetch_aiohttp_cache.sqlite",
+    expire_after=timedelta(days=2),
+    urls_expire_after={
+        f"{BASE_URL}/drivers": timedelta(weeks=1),
+        f"{BASE_URL}/drivers/*": 3600,
+        f"{BASE_URL}/current/last/*": 600,
+        f"{BASE_URL}/current/next": 600,
+    },
+    allowed_methods=("GET", "POST"),
+)
 
 
-def is_json(res): return 'application/json' in res.content_type
+def _is_xml(res): return 'application/xml' in res.content_type
 
 
-async def send_request(session, url):
+def _is_json(res): return 'application/json' in res.content_type
+
+
+async def _send_request(session, url):
     """Attempt to request the URL. Returns content of the Response if successful or None."""
     logger.info('GET {}'.format(url))
     # open connection context, all response handling must be within
@@ -26,9 +47,9 @@ async def send_request(session, url):
             return None
         # check response type, file streaming should be handled seperately
         else:
-            if is_xml(res):
+            if _is_xml(res):
                 content = await res.read()
-            elif is_json(res):
+            elif _is_json(res):
                 content = await res.json()
             else:
                 content = await res.text()
@@ -39,9 +60,15 @@ async def fetch(url):
     """Request the url and await response. Returns response content or None."""
     tmout = aiohttp.ClientTimeout(total=SESSION_TIMEOUT)
     try:
-        async with aiohttp.ClientSession(timeout=tmout) as session:
-            res = await send_request(session, url)
-            return res
+        async with CachedSession(cache=cache, timeout=tmout) as session:
+            if use_cache:
+                return await _send_request(session, url)
+
+            # Temporarily disable cache for this request
+            async with session.disabled():
+                uncached_res = await _send_request(session, url)
+                return uncached_res
+
     except aiohttp.ClientError as e:
         logger.error(e)
         return None
