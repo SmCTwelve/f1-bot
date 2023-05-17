4c554ae2d3d4a9a34adcc5acee1adeebec2c57d2
SmCTwelve <smctwelve@gmail.com>
2023-05-17 04:13:44 +0100

    Remove deprecated logger.warn use

--- main.py
+++ main.py
@@ -4,12 +4,11 @@ import os
 
 from f1.config import Config
 from f1 import commands  # noqa
-from f1 import admin  # noqa
 
 dotenv.load_dotenv()
 cfg = Config()
 
 logger = logging.getLogger("f1-bot")
-logger.warn('Starting bot...')
+logger.warning("Starting bot...")
 
-cfg.bot.run(os.getenv('BOT_TOKEN'))
+cfg.bot.run(os.getenv("BOT_TOKEN"))
