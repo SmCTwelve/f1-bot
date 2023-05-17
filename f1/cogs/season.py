5e3b234142eb06e7ecfdf6abb531c271fe88f46d
SmCTwelve <smctwelve@gmail.com>
2023-05-17 23:06:34 +0100

    Rename season option to year

--- f1/cogs/season.py
+++ f1/cogs/season.py
@@ -21,15 +21,15 @@ class Season(commands.Cog, guild_ids=Config().guilds):
         self.bot = bot
 
     @commands.slash_command(description="Driver championship standings.")
-    async def wdc(self, ctx, season: options.SeasonOption):
+    async def wdc(self, ctx, year: options.SeasonOption):
         """Display the Driver Championship standings as of the last race or `season`.
 
         Usage:
         ------
             /wdc [season]    WDC standings from [season].
         """
-        await check_season(ctx, season)
-        result = await ergast.get_driver_standings(season)
+        await check_season(ctx, year)
+        result = await ergast.get_driver_standings(year)
         table = make_table(result['data'], fmt='simple')
         await MessageTarget(ctx).send(
             embed=Embed(
@@ -40,15 +40,15 @@ class Season(commands.Cog, guild_ids=Config().guilds):
         )
 
     @commands.slash_command(description="Constructors Championship standings.")
-    async def wcc(self, ctx, season: options.SeasonOption):
+    async def wcc(self, ctx, year: options.SeasonOption):
         """Display Constructor Championship standings as of the last race or `season`.
 
         Usage:
         ------
             /wcc [season]   WCC standings from [season].
         """
-        await check_season(ctx, season)
-        result = await ergast.get_team_standings(season)
+        await check_season(ctx, year)
+        result = await ergast.get_team_standings(year)
         table = make_table(result['data'])
         await MessageTarget(ctx).send(
             embed=Embed(
@@ -59,7 +59,7 @@ class Season(commands.Cog, guild_ids=Config().guilds):
         )
 
     @commands.slash_command(desciption="All drivers and teams participating in the season.")
-    async def grid(self, ctx, season: options.SeasonOption):
+    async def grid(self, ctx, year: options.SeasonOption):
         """Display all the drivers and teams participating in the `season`.
 
         Usage:
@@ -67,8 +67,8 @@ class Season(commands.Cog, guild_ids=Config().guilds):
             /grid            All drivers and teams in the current season as of the last race.
             /grid [season]   All drivers and teams at the end of [season].
         """
-        await check_season(ctx, season)
-        result = await ergast.get_all_drivers_and_teams(season)
+        await check_season(ctx, year)
+        result = await ergast.get_all_drivers_and_teams(year)
         table = make_table(sorted(result['data'], key=itemgetter('Team')), fmt='simple')
         await MessageTarget(ctx).send(
             content=f"```\n{table}\n```",
